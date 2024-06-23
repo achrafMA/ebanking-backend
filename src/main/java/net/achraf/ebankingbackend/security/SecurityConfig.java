5d8f0cc382a1a745c371e396ae9ea0e029770e21
achrafMA <achrafmaataoui218@gmail.com>
2024-06-23 02:01:10 +0100

    Encoder

--- src/main/java/net/achraf/ebankingbackend/security/SecurityConfig.java
+++ src/main/java/net/achraf/ebankingbackend/security/SecurityConfig.java
@@ -1,19 +1,33 @@
 package net.achraf.ebankingbackend.security;
 
+import com.nimbusds.jose.jwk.source.ImmutableSecret;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.crypto.spec.SecretKeySpec;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Value("$(jwt.secret)")
+    private String secretKey;
     @Bean
     public InMemoryUserDetailsManager inMemoryUserDetailsManager(){
         PasswordEncoder passwordEncoder=passwordEncoder();
@@ -32,6 +46,19 @@ public class SecurityConfig {
                 .sessionManagement(sm ->sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS) )
                 .csrf(crsf ->crsf.disable())
                 .authorizeHttpRequests(ar ->ar.anyRequest().authenticated() )
+                .oauth2ResourceServer(oa ->oa.jwt(Customizer.withDefaults()))
                 .build();
+    }
+
+    @Bean
+    JwtEncoder jwtEncoder(){
+        //String secretKey="JeNmJbFzqjc9zeD5d7Ybjc%$GpAf@d#59%z9E5pKhU78b4g7RhkeNbu8PNPTzwWe";
+        return new NimbusJwtEncoder(new ImmutableSecret<>(secretKey.getBytes()));
+    }
+    @Bean
+    JwtDecoder jwtDecoder(){
+        //String secretKey="JeNmJbFzqjc9zeD5d7Ybjc%$GpAf@d#59%z9E5pKhU78b4g7RhkeNbu8PNPTzwWe";
+        SecretKeySpec secretKeySpec= new SecretKeySpec(secretKey.getBytes(),"RSA");
+        return NimbusJwtDecoder.withSecretKey(secretKeySpec).macAlgorithm(MacAlgorithm.HS512).build();
     }
 }
